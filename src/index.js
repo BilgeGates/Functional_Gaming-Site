87e865a435ef2db9492e14457e55c862ee04d4b9
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-01-08 21:07:13 +0400

    rename file and folder name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./Routes/Router";
+import router from "./routes/router";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
